5e504c522d7f084a92830ff5fb26cc4d8090e96a
The-Old-Crow <matt.mines44@gmail.com>
2020-03-04 15:02:17 +0000

    added basic enemies

--- maze.py
+++ maze.py
@@ -6,7 +6,9 @@ win = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 pygame.display.set_caption("Simple Maze")
 wwin, hwin = pygame.display.get_surface().get_size()
 
+red = (255,0,0)
 green = (0,255,0)
+blue = (0,0,255)
 black = (0,0,0)
 white = (255,255,255)
 
@@ -19,7 +21,7 @@ class Wall(pygame.sprite.Sprite):
 	def __init__(self, x, y, w, h):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.Surface([w, h])
-		self.image.fill(white)
+		self.image.fill(blue)
 		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
@@ -39,7 +41,7 @@ class Player(pygame.sprite.Sprite):
 		self.rect.y = y
 		self.speed = 5
 	
-	def draw(self, wall):
+	def move(self, wall):
 		self.rect.x += (self.speed * self.velocity[0])
 
 		wall_collisions = pygame.sprite.spritecollide(self, wall_group, False)
@@ -61,9 +63,62 @@ class Player(pygame.sprite.Sprite):
 			else:
 				self.rect.top = wall.rect.bottom
 
+class Enemy(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([30,30])
+		self.image.fill(red)
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.xdirection, self.ydirection = 0, 0
+		self.velocity =(self.xdirection, self.ydirection)
+		self.speed = 3
+		
+	def move(self, target, wall):
+		# Decide which direction to move...
+		if target.rect.x > self.rect.x:
+			self.xdirection = 1
+		elif target.rect.x < self.rect.x:
+			self.xdirection = -1
+		else:
+			self.xdirection = 0
+
+		if target.rect.y > self.rect.y:
+			self.ydirection = 1
+		elif target.rect.y < self.rect.y:
+			self.ydirection = -1
+		else:
+			self.ydirection = 0
+
+		# Make necessary coordinate changes
+		self.velocity = (self.xdirection, self.ydirection)
+		self.rect.x += (self.speed * self.velocity[0])
+		wall_collisions = pygame.sprite.spritecollide(self, wall_group, False)
+		for wall in wall_collisions:
+			if self.velocity[0] > 0:
+				self.rect.right = wall.rect.left
+			else:
+				self.rect.left = wall.rect.right
+
+		self.rect.y += (self.speed * self.velocity[1])
+		wall_collisions = pygame.sprite.spritecollide(self, wall_group, False)
+		for wall in wall_collisions:
+			if self.velocity[1] > 0:
+				self.rect.bottom = wall.rect.top
+			else:
+				self.rect.top = wall.rect.bottom
+
 
 player = Player(50, 50, 20, 20)
 all_sprites.add(player)
+
+enemy1 = Enemy(100, 100)
+enemy2 = Enemy(400, 400)
+enemy3 = Enemy(700, 500)
+all_sprites.add(enemy1)
+all_sprites.add(enemy2)
+all_sprites.add(enemy3)
 
 left_wall = Wall(0, 0, 10, hwin)
 wall_group.add(left_wall)
@@ -85,18 +140,18 @@ middle_wall = Wall(((wwin//2)-10), 0, 10, (hwin//2))
 wall_group.add(middle_wall)
 all_sprites.add(middle_wall)
 
-for i in range(0, 150, 1):
+for i in range(0, 100, 1):
 	#random start point
 	randomstartx = random.randrange(0, wwin, 20)
 	randomstarty = random.randrange(0, hwin, 20)
 	#horizontal or vertical
 	orientation = random.randrange(-1, 2, 1)
 	if orientation > 0:
-		randomwall = Wall(randomstartx, randomstarty, 10, 200)
+		randomwall = Wall(randomstartx, randomstarty, 10, 300)
 		wall_group.add(randomwall)
 		all_sprites.add(randomwall)
 	else:
-		randomwall = Wall(randomstartx, randomstarty, 200, 10)
+		randomwall = Wall(randomstartx, randomstarty, 300, 10)
 		wall_group.add(randomwall)
 		all_sprites.add(randomwall)
 running = True
@@ -123,7 +178,10 @@ while running:
 	if keys[pygame.K_RIGHT]:
 		player.velocity = (1, 0)
 		
-	player.draw(wall_group)
+	player.move(wall_group)
+	enemy1.move(player, wall_group)
+	enemy2.move(player, wall_group)
+	enemy3.move(player, wall_group)
 	win.fill(black)
 	all_sprites.draw(win)
 	pygame.display.flip()
